53ba3fd84e4d0144287d4dbd09ead3e5cee79bd8
Ballardin <gballardin@lm-sjc-11001372.attlocal.net>
2017-09-27 09:10:15 -0700

    latest changes

--- timeusage/src/main/scala/timeusage/TimeUsage.scala
+++ timeusage/src/main/scala/timeusage/TimeUsage.scala
@@ -89,11 +89,14 @@ object TimeUsage {
     * 2. working activities. These are the columns starting with “t05” and “t1805”.
     * 3. other activities (leisure). These are the columns starting with “t02”, “t04”, “t06”, “t07”, “t08”, “t09”,
     *    “t10”, “t12”, “t13”, “t14”, “t15”, “t16” and “t18” (those which are not part of the previous groups only).
-
-  def classifiedColumns(columnNames: List[String]): (List[Column], List[Column], List[Column]) = {
-    ???
-  }
     */
+  def classifiedColumns(columnNames: List[String]): (List[Column], List[Column], List[Column]) = {
+    val regexFirst = "".r
+    val regexSecond = "t05.*|t1805.*".r
+    columnNames filter (x => regexSecond.pattern.matcher(x).matches)
+    columnNames.drop(1)
+  }
+
   /** @return a projection of the initial DataFrame such that all columns containing hours spent on primary needs
     *         are summed together in a single column (and same for work and leisure). The “teage” column is also
     *         projected to three values: "young", "active", "elder".
